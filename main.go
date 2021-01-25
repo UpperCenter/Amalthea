d0d39e6350db26aaf01beb730c6b3a5adcaee5cb
Tom Hindby <46819924+UpperCenter@users.noreply.github.com>
2021-01-25 20:46:06 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-/*
-
-	Amalthea Ransomware.
-	Developer for IY3D602 - Advanced Systems Security
-	Download: https://cyber.lol/dl/Amalthea.exe
-
-	Use:
-		Obfuscated: `env GOOS=windows GOARCH=amd64 garble -seed=random -literals -debugdir=out build .`
-		Obfuscated with tiny: `env GOOS=windows GOARCH=amd64 garble -tiny -seed=random -literals -debugdir=out build .`
-		Normal: `env GOOS=windows GOARCH=amd64 go build .`
-
-	Notes on `-tiny`
-	> When the -tiny flag is passed, extra information is stripped from the resulting Go binary.
-	> This includes line numbers, filenames, and code in the runtime the prints panics, fatal errors, and trace/debug info.
-
-*/
-
 // Import all the required packages
 import (
 	"fmt"
